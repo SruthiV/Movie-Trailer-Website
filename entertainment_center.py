ad8f3d10675eb0fdd0819e7845d026925f36ef18
Sruthi <vinukonda.sruthi@gmail.com>
2017-08-08 16:28:30 -0500

    Add files via upload

--- entertainment_center.py
+++ entertainment_center.py
@@ -21,7 +21,25 @@ the_martian = media.Movie(
     "https://goo.gl/TQsKNK",
     "https://www.youtube.com/watch?v=ej3ioOneTy8")
 
-movies = [spotlight, titanic, the_martian]
+jurassic_park = media.Movie(
+    "Jurassic Park",
+    "A science fiction adventure film about cloned dinosaurs.",
+    "https://goo.gl/y26nSP",
+    "https://www.youtube.com/watch?v=lc0UehYemQA")
+
+lion_king = media.Movie(
+    "Lion King",
+    "Animated movie about Simba, a young lion.",
+    "https://goo.gl/WphtiF",
+    "https://www.youtube.com/watch?v=4sj1MT05lAA")
+
+the_jungle_book = media.Movie(
+    "The Jungle Book",
+    "Animated movie about Mowgli, a feral child.",
+    "https://goo.gl/TkGdnT",
+    "https://www.youtube.com/watch?v=LNVTKXIK7q8")
+
+movies = [spotlight, titanic, the_martian, jurassic_park, lion_king, the_jungle_book]
 
 # Function to build the HTML file
 
